0c5217fe381b82368f3925d9b8d86cadbea46fe2
Juan Ramirez <jramirez203@outlook.com>
2025-07-13 13:21:50 -0500

    feat: Add favicon and metadata images

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,26 @@ import './globals.css'
 import { Inter, Source_Serif_4 } from 'next/font/google'
 import Header from './components/Header'
 
+const siteUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+
 export const metadata: Metadata = {
+  metadataBase: new URL(siteUrl),
   title: 'Juan Ramirez | Portfolio',
   description: 'The portfolio of Juan Ramirez, a software developer specializing in scalable web applications.',
+  openGraph: {
+    images: ['/metadata-icon.png'],
+  },
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', type: 'image/png', sizes: '180x180' },
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
 }
 
 const inter = Inter({
